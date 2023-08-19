7495ee1fe6fca123775bc4cd82ecb5c66df3dffd
ZubriQ <zubrich157@gmail.com>
2023-08-19 17:57:51 +0700

    Complete task - add another component

--- Tasks/08-reusable-components/src/App.js
+++ Tasks/08-reusable-components/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import MyTextComponent from './components/MyTextComponent';
 
 function App() {
   return (
     <div className="App">
       <MyComponent />
+      <MyTextComponent />
       <MyComponent />
+      <MyTextComponent />
     </div>
   );
 }
---
+++ Tasks/08-reusable-components/src/components/MyTextComponent.js
@@ -0,0 +1,5 @@
+function MyTextComponent() {
+  return <p>Text from MyTextComponent</p>;
+}
+
+export default MyTextComponent;
