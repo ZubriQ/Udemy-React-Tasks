d4e369d11ea135cfe2bedef0f0d645bb6178f75c
ZubriQ <zubrich157@gmail.com>
2023-08-20 16:42:55 +0700

    Add children access parent's state

--- Tasks/12-state-via-props/src/App.js
+++ Tasks/12-state-via-props/src/App.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
+import Counter from './components/Counter';
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="App">
+      <Counter count={count} />
+      <Button setCount={incrementCount} />
+      <Button setCount={incrementCount} />
+      <Button setCount={incrementCount} />
+      <Button setCount={incrementCount} />
+    </div>
+  );
 }
 
 export default App;
---
+++ Tasks/12-state-via-props/src/components/Button.js
@@ -0,0 +1,5 @@
+function Button({ setCount }) {
+  return <button onClick={setCount}>Click me!</button>;
+}
+
+export default Button;
