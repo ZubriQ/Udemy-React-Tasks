bbc73c3525361c2318d65779cb513916053ad683
ZubriQ <zubrich157@gmail.com>
2023-08-21 16:28:47 +0700

    Refactor person cards

--- Tasks/14-map-array-of-objects/src/App.js
+++ Tasks/14-map-array-of-objects/src/App.js
@@ -1,30 +1,10 @@
 import './App.css';
-import Person from './components/Person';
-import persons from './data/persons';
+import Persons from './components/Persons';
 
 function App() {
   return (
     <div className="App">
-      {persons.map((person) => {
-        //const { id, firstName, lastName, email, img } = person;
-        // return (
-        //   <h4>
-        //     <Person
-        //       id={id}
-        //       firstName={firstName}
-        //       lastName={lastName}
-        //       email={email}
-        //       img={img}
-        //     />
-        //   </h4>
-        // );
-        return (
-          <h4>
-            {/* React needs 'key' prop to know which object to rerender */}
-            <Person key={person.id} {...person} />
-          </h4>
-        );
-      })}
+      <Persons />
     </div>
   );
 }
