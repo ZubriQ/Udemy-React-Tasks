fcef08dcd6e858573377fcaf52c6b4d8ee0ec67a
ZubriQ <zubrich157@gmail.com>
2023-08-21 16:02:57 +0700

    Add persons mocking using spread operator

--- Tasks/14-map-array-of-objects/src/App.js
+++ Tasks/14-map-array-of-objects/src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
+import Person from './components/Person';
+import persons from './data/persons';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {persons.map((person) => {
+        //const { id, firstName, lastName, email, img } = person;
+        // return (
+        //   <h4>
+        //     <Person
+        //       id={id}
+        //       firstName={firstName}
+        //       lastName={lastName}
+        //       email={email}
+        //       img={img}
+        //     />
+        //   </h4>
+        // );
+        return (
+          <h4>
+            {/* React needs 'key' prop to know which object to rerender */}
+            <Person key={person.id} {...person} />
+          </h4>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ Tasks/14-map-array-of-objects/src/components/Person.js
@@ -0,0 +1,5 @@
+function Person({ id, firstName, lastName, email, img }) {
+  return <div></div>;
+}
+
+export default Person;
