f11c1f7e9db8b50937e927b1836639766ec7e7a9
ZubriQ <zubrich157@gmail.com>
2023-08-22 20:32:21 +0700

    Implement Wrapper component with children

--- Tasks/19-children-prop/src/App.js
+++ Tasks/19-children-prop/src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import Wrapper from './components/Wrapper';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Wrapper color="lightblue">
+        <h2>Text insdide of the Wrapper</h2>
+        <button>Click me</button>
+      </Wrapper>
+      <Wrapper color="purple">
+        <h2>Another text</h2>
+        <p>Description</p>
+        <input type="text" placeholder="Enter value"></input>
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
