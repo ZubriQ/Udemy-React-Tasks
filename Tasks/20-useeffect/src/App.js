01b6e47e671f5a96e46aad8789b7e7ec5e59ba55
ZubriQ <zubrich157@gmail.com>
2023-08-22 22:54:08 +0700

    Implement useEffect with fetch

--- Tasks/20-useeffect/src/App.js
+++ Tasks/20-useeffect/src/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  const [todo, setTodo] = useState(null);
 
-  return <div className="App"></div>;
+  useEffect(
+    () => {
+      console.log('useEffect called');
+      fetch('https://jsonplaceholder.typicode.com/todos/7')
+        .then((response) => response.json())
+        .then((json) => setTodo(json));
+    },
+    [
+      /* callback function could be used every time a prop updated in this array*/
+    ]
+  );
+
+  console.log('App rendered');
+  console.log(todo);
+
+  return <div className="App">{todo && <h1>{todo.title}</h1>}</div>;
 }
 
 export default App;
