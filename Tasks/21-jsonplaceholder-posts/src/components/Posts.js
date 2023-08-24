e4f06cd8fa64623025a1ce8467cc7c9bba5776b2
ZubriQ <zubrich157@gmail.com>
2023-08-24 19:37:58 +0700

    Implement error state

--- Tasks/21-jsonplaceholder-posts/src/components/Posts.js
+++ Tasks/21-jsonplaceholder-posts/src/components/Posts.js
@@ -3,13 +3,18 @@ import Post from './Post';
 
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(''); // '' equals false
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((apiPosts) => setPosts(apiPosts))
-      .catch((error) => console.log(error.message));
+      .catch((error) => setError(error.message));
   }, []);
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
 
   return (
     <div>
