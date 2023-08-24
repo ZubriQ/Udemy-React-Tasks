0f64faef1d7f6f18fe8d70aba490947bc12f7d34
ZubriQ <zubrich157@gmail.com>
2023-08-24 19:45:32 +0700

    Add is loading state

--- Tasks/21-jsonplaceholder-posts/src/components/Posts.js
+++ Tasks/21-jsonplaceholder-posts/src/components/Posts.js
@@ -4,13 +4,19 @@ import Post from './Post';
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(''); // '' equals false
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((apiPosts) => setPosts(apiPosts))
-      .catch((error) => setError(error.message));
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
   }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
   if (error) {
     return <h1>Error: {error}</h1>;
