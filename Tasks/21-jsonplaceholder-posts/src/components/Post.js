30e6b3213ba45bc429bb4b6dc8f16e49522086be
ZubriQ <zubrich157@gmail.com>
2023-08-24 19:27:23 +0700

    Style posts

--- Tasks/21-jsonplaceholder-posts/src/components/Post.js
+++ Tasks/21-jsonplaceholder-posts/src/components/Post.js
@@ -1,10 +1,12 @@
 function Post({ userId, id, title, body }) {
   return (
     <div>
-      <h1>{userId}</h1>
-      <h2>{id}</h2>
-      <h4>{title}</h4>
-      <p>{body}</p>
+      <h1>User {userId}</h1>
+      <h1>Post {id}</h1>
+      <h2>{title}</h2>
+      <p>
+        <i>Comment:</i> {body}
+      </p>
     </div>
   );
 }
