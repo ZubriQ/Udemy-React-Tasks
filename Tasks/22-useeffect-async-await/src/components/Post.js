76da61cff1368abff367ccb912e9dc1de5133988
ZubriQ <zubrich157@gmail.com>
2023-08-24 19:59:28 +0700

    Add project 22 - useEffect async await based on project 21

---
+++ Tasks/22-useeffect-async-await/src/components/Post.js
@@ -0,0 +1,16 @@
+import './Post.css';
+
+function Post({ userId, id, title, body }) {
+  return (
+    <div className="post">
+      <h1>User {userId}</h1>
+      <h1>Post {id}</h1>
+      <h2>{title}</h2>
+      <p>
+        <i>Comment:</i> {body}
+      </p>
+    </div>
+  );
+}
+
+export default Post;
