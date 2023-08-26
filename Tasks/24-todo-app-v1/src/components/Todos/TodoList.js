610b137c1ddf7966f87f6f92b92c3e78cead8b09
ZubriQ <zubrich157@gmail.com>
2023-08-27 01:50:06 +0700

    Refactor if statement with && operator

--- Tasks/24-todo-app-v1/src/components/Todos/TodoList.js
+++ Tasks/24-todo-app-v1/src/components/Todos/TodoList.js
@@ -2,12 +2,9 @@ import styles from './TodoList.module.css';
 import Todo from './Todo';
 
 function TodoList({ todos }) {
-  if (todos.length === 0) {
-    return <h2>The todo list is empty</h2>;
-  }
-
   return (
     <div className={styles.todoList}>
+      {todos.length === 0 && <h2>The todo list is empty</h2>}
       {todos.map((todo, index) => (
         <Todo key={index} todo={todo} />
       ))}
