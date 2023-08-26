d4555ae6634481a2c0cb6a844bd432313a2d165e
ZubriQ <zubrich157@gmail.com>
2023-08-26 18:21:13 +0700

    Refactor todo logic

--- Tasks/24-todo-app-v1/src/App.js
+++ Tasks/24-todo-app-v1/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import Todo from './components/Todos/Todo';
+import TodoForm from './components/Todos/TodoForm';
+import TodoList from './components/Todos/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      <h1>
+        Hello, this is my Todo app, which is made with my React programming
+        skills
+      </h1>
+      <TodoForm />
+      <TodoList></TodoList>
     </div>
   );
 }
--- Tasks/24-todo-app-v1/src/components/Todos/Todo.js
+++ Tasks/24-todo-app-v1/src/components/Todos/Todo.js
@@ -1,14 +1,5 @@
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
-
 function Todo() {
-  return (
-    <>
-      <h1>Hello, this is my Todo app which is made with my knowledge</h1>
-      <TodoForm />
-      <TodoList></TodoList>
-    </>
-  );
+  return <div></div>;
 }
 
 export default Todo;
