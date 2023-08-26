c02ee7bcd3374cfb2bda611c894c69f870ebf408
ZubriQ <zubrich157@gmail.com>
2023-08-27 01:47:38 +0700

    Implement check if the todo list is empty

--- Tasks/24-todo-app-v1/src/components/Todos/TodoList.js
+++ Tasks/24-todo-app-v1/src/components/Todos/TodoList.js
@@ -2,6 +2,10 @@ import styles from './TodoList.module.css';
 import Todo from './Todo';
 
 function TodoList({ todos }) {
+  if (todos.length === 0) {
+    return <h2>The todo list is empty</h2>;
+  }
+
   return (
     <div className={styles.todoList}>
       {todos.map((todo, index) => (
