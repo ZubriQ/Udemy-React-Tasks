ed28325716ec156036ad68a88fdb34bdf51d0f4f
ZubriQ <zubrich157@gmail.com>
2023-08-27 02:09:05 +0700

    Implement react font awesome icon

--- Tasks/24-todo-app-v1/src/components/Todos/Todo.js
+++ Tasks/24-todo-app-v1/src/components/Todos/Todo.js
@@ -1,13 +1,10 @@
+import { FaIoxhost } from 'react-icons/fa';
 import styles from './Todo.module.css';
 
 function Todo({ todo, index, deleteTodo }) {
   return (
     <div className={styles.todoCard} onDoubleClick={() => deleteTodo(index)}>
-      <img
-        src={`${process.env.PUBLIC_URL}/favicon-32x32.png`}
-        alt="icon"
-        className={styles.todoIcon}
-      />
+      <FaIoxhost className={styles.todoIcon} />
       <p>{todo}</p>
     </div>
   );
