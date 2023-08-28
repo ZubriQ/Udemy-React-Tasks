76348ad667be34870f8ad84a76069fe77a925e02
ZubriQ <zubrich157@gmail.com>
2023-08-28 17:13:45 +0700

    Add project 27 - create courses task

---
+++ Tasks/27-create-courses-task/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import MainLayout from './layouts/MainLayout';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="about" element={<About />} />
+            <Route path="contacts" element={<Contacts />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Tasks/27-create-courses-task/src/components/Menu.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <nav>
+      <NavLink
+        to="."
+        end
+        // Option 3 that shows how we can update css:
+        style={({ isActive }) =>
+          isActive ? { color: '#95d779', textDecoration: 'none' } : {}
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="about"
+        className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="contacts"
+        className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+      >
+        Contacts
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Menu;
