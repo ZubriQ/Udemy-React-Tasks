8e96100e6fccec60ffb2bade33d1e603f9bf74eb
ZubriQ <zubrich157@gmail.com>
2023-08-29 02:52:11 +0700

    Implement courses component

--- Tasks/26-react-router/src/components/Courses.jsx
+++ Tasks/26-react-router/src/components/Courses.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import courses from '../data/courses';
+
 const Courses = () => {
-  return <h1>Courses</h1>;
+  return (
+    <>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <Link to={course.slug} className="courseLink">
+            {course.title}
+          </Link>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Courses;
