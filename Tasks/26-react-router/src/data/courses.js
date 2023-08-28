c4661aca5bd6aa03959a9b478f1e205cad1f6ee5
ZubriQ <zubrich157@gmail.com>
2023-08-28 17:05:35 +0700

    Add courses data

---
+++ Tasks/26-react-router/src/data/courses.js
@@ -0,0 +1,24 @@
+const courses = [
+  {
+    id: 357,
+    title: 'Complete React Guide',
+    slug: 'react',
+  },
+  {
+    id: 358,
+    title: 'Complete JavaScript Guide',
+    slug: 'javascript',
+  },
+  {
+    id: 359,
+    title: 'Complete Python Guide',
+    slug: 'python',
+  },
+  {
+    id: 450,
+    title: 'Complete Tea Brewing Guide',
+    slug: 'tea-brewing',
+  },
+];
+
+export default courses;
