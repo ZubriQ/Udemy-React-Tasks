e566874cfde5be42e829ef12ea29a89b9e214b20
ZubriQ <zubrich157@gmail.com>
2023-08-29 19:57:36 +0700

    Implement sorting

--- Tasks/26-react-router/src/components/Courses.jsx
+++ Tasks/26-react-router/src/components/Courses.jsx
@@ -1,10 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import courses from '../data/courses';
+import { useState } from 'react';
+
+function sortCourses(courses, key) {
+  if (!key) {
+    return courses;
+  }
+
+  const sortedCourses = [...courses];
+  sortedCourses.sort((a, b) => (a[key] < b[key] ? 1 : -1));
+  return sortedCourses;
+}
 
 const Courses = () => {
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const [sortKey, setSortKey] = useState(query.sort);
+  const [sortedCourses, setSortedCourses] = useState(
+    sortCourses(courses, sortKey)
+  );
+
   return (
     <>
-      {courses.map((course) => (
+      <h1>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h1>
+      {sortedCourses.map((course) => (
         <div key={course.id}>
           <Link to={course.slug} className="courseLink">
             {course.title}
