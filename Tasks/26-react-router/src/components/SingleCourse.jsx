11e0a2526991ea46495666f2b9d0edc9a5cd2a8e
ZubriQ <zubrich157@gmail.com>
2023-08-29 15:22:28 +0700

    Implement single course component

--- Tasks/26-react-router/src/components/SingleCourse.jsx
+++ Tasks/26-react-router/src/components/SingleCourse.jsx
@@ -1,5 +1,22 @@
+import { Link, useParams } from 'react-router-dom';
+import courses from '../data/courses';
+
 const SingleCourse = () => {
-  return <h1>Single Course Info</h1>;
+  const params = useParams();
+  const course = courses.find((course) => course.slug === params.courseSlug);
+  return (
+    <>
+      <h1>{course.title}</h1>
+      <h2>{course.slug}</h2>
+      <h3>{course.id}</h3>
+      {/*Option 1: Absolute link*/}
+      {/*<Link to="/courses">All courses</Link>*/}
+      {/*Option 2: It's advised to use relative links*/}
+      <Link to=".." relative="path">
+        All courses
+      </Link>
+    </>
+  );
 };
 
 export default SingleCourse;
