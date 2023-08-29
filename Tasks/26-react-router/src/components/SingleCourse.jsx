ed675edbc927f7faed27e263e03de2078d339346
ZubriQ <zubrich157@gmail.com>
2023-08-29 15:52:51 +0700

    Implement useNavigate with useEffect instead of returning NotFound component

--- Tasks/26-react-router/src/components/SingleCourse.jsx
+++ Tasks/26-react-router/src/components/SingleCourse.jsx
@@ -1,19 +1,31 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import courses from '../data/courses';
 import NotFound from './NotFound';
+import { useEffect } from 'react';
 
 const SingleCourse = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const course = courses.find((course) => course.slug === params.courseSlug);
-  if (!course) {
-    return <NotFound />;
-  }
+
+  useEffect(() => {
+    if (!course) {
+      navigate('..', { relative: 'path' });
+    }
+    // Also don't forget the array of dependencies
+  }, [course]);
+
+  // Option 2:
+  // Showing NotFound component
+  // if (!course) {
+  //   return <NotFound />;
+  // }
 
   return (
     <>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h3>{course.id}</h3>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
       {/*Option 1: Absolute link*/}
       {/*<Link to="/courses">All courses</Link>*/}
       {/*Option 2: It's advised to use relative links*/}
