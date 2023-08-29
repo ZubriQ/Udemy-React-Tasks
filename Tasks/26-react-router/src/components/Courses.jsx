044cfa4594bc5410fd544778c2a044e8a488fe57
ZubriQ <zubrich157@gmail.com>
2023-08-29 20:09:28 +0700

    Implement key check and redirecting with useNavigate

--- Tasks/26-react-router/src/components/Courses.jsx
+++ Tasks/26-react-router/src/components/Courses.jsx
@@ -1,10 +1,12 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import courses from '../data/courses';
-import { useState } from 'react';
+
+const SORT_KEYS = ['title', 'slug', 'id'];
 
 function sortCourses(courses, key) {
-  if (!key) {
+  if (!key || !SORT_KEYS.includes(key)) {
     return courses;
   }
 
@@ -16,14 +18,26 @@ function sortCourses(courses, key) {
 const Courses = () => {
   const location = useLocation();
   const query = queryString.parse(location.search);
+  const navigate = useNavigate();
   const [sortKey, setSortKey] = useState(query.sort);
   const [sortedCourses, setSortedCourses] = useState(
     sortCourses(courses, sortKey)
   );
 
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey)) {
+      navigate('.');
+      setSortKey();
+    }
+  }, [sortKey, navigate]);
+
   return (
     <>
-      <h1>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h1>
+      <h1>
+        {sortKey && SORT_KEYS.includes(sortKey)
+          ? `Courses sorted by ${sortKey}`
+          : 'Courses'}
+      </h1>
       {sortedCourses.map((course) => (
         <div key={course.id}>
           <Link to={course.slug} className="courseLink">
